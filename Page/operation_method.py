a072b114ff375b4b4cc2759c7d8981ee40cf811b
Cz166 <Cz16601035178@163.com>
2018-08-04 16:23:27 +0800

    '更新'

--- Page/operation_method.py
+++ Page/operation_method.py
@@ -1,4 +1,6 @@
 import time
+
+import allure
 from appium.webdriver.common.touch_action import TouchAction
 
 from Base.Base import Base
@@ -11,20 +13,20 @@ class method_001(Base):
     """
     常用滑动屏幕
     """
-    # 向右滑动
+    @allure.attach('描述','滑动屏幕向右')
     def right_downward_slide(self):
         TouchAction(self.driver).press(x=965, y=1666).move_to(x=77, y=1582).release().perform()
-    # 向上滑动屏幕
+    @allure.attach('描述','滑动屏幕向上')
     def up_slide(self):
         TouchAction(self.driver).press(x=481, y=1672).move_to(x=504, y=691).release().perform()
-    # 向下滑动屏幕
+    @allure.attach('描述','滑动屏幕向下')
     def below_slide(self):
         TouchAction(self.driver).press(x=504, y=691).move_to(x=481, y=1672).release().perform()
 
     """
     截图
     """
-    # 截图操作
+    @allure.attach('描述','屏幕截图操作')
     def screenshot(self):
         now = time.strftime('%Y_%m_%d-%H_%M_%S')
         return self.driver.get_screenshot_as_file \
@@ -33,29 +35,35 @@ class method_001(Base):
     """
     初始页面
     """
-    # 点击启动页面_教程_进入爱优品按钮
+
+    @allure.attach('描述','点击启动页面_教程_进入爱优品按钮')
     def click_access_love_youpin(self):
         self.click_element(Page.access_love_youpin)
-    # 点击始终允许
+
+    @allure.attach('描述','点击始终允许')
     def click_allow_button(self):
         self.click_element(Page.allow_button)
 
     """
     个人中心
     """
-    # 点击_我的
+    @allure.attach('描述','点击_我的')
     def click_my_button(self):
         self.click_element(Page.my_button)
-    # 点击登录、个人中心入口按钮
+
+    @allure.attach('描述','点击登录、个人中心入口按钮')
     def click_register(self):
         self.click_element(Page.personal_center)
-    # 点击退出登录按钮
+
+    @allure.attach('描述','点击退出登录按钮')
     def click_quit_register(self):
         self.click_element(Page.quit_register)
-    # 点击退出登录_确定按钮
+
+    @allure.attach('描述','点击退出登录_确定按钮')
     def click_quit_is_register(self):
         self.click_element(Page.quit_is_register)
-    # 点击登录页面_确定按钮
+
+    @allure.attach('描述','点击登录页面_确定按钮')
     def click_register_confirm(self):
         self.click_element(Page.register_confirm)
 
@@ -63,49 +71,51 @@ class method_001(Base):
     """
     我的订单
     """
+
+    @allure.attach('描述','点击我的订单按钮')
     def click_my_indent(self):
-        # 点击我的订单按钮
         self.click_element(Page.my_indent)
+    @allure.attach('描述','我的订单_点击返回按钮')
     def click_return_button(self):
-        # 我的订单_点击返回按钮
         self.click_element(Page.return_button)
+    @allure.attach('描述','点击我发布的按钮')
     def click_I_issue(self):
-        # 点击我发布的按钮
         self.click_element(Page.I_issue)
+    @allure.attach('描述','点击官方回收按钮')
     def click_official_recycle(self):
-        # 点击官方回收按钮
         self.click_element(Page.official_recycle)
+    @allure.attach('描述','点击我的收藏')
     def click_my_collect(self):
-        # 点击我的收藏
         self.click_element(Page.my_collect)
+    @allure.attach('描述','点击我的关注')
     def click_my_attention(self):
-        # 点击我的关注
         self.click_element(Page.my_attention)
+    @allure.attach('描述','点击粉丝')
     def click_fans(self):
-        # 点击粉丝
         self.click_element(Page.fans)
+    @allure.attach('描述','点击我的消息')
     def click_news(self):
-        # 点击我的消息
         self.click_element(Page.my_news)
+    @allure.attach('描述',' 点击我的评论')
     def click_my_comment(self):
-        # 点击我的评论
         self.click_element(Page.my_comment)
+    @allure.attach('描述','点击系统消息')
     def click_system_news(self):
-        # 点击系统消息
         self.click_element(Page.system_news)
+    @allure.attach('描述','点击我的红包')
     def click_my_red_packet(self):
-        # 点击我的红包
         self.click_element(Page.my_red_packet)
+    @allure.attach('描述','点击隐私设置')
     def click_privacy_set(self):
-        # 点击隐私设置
         self.click_element(Page.privacy_set)
+    @allure.attach('描述','点击电话隐私')
     def click_phone_privacy(self):
-        # 点击电话隐私
         self.click_element(Page.phone_privacy)
+    @allure.attach('描述','点击版本更新')
     def click_release_update(self):
-        # 点击版本更新
         self.click_element(Page.release_update)
     # 常用方法_正确帐号登录（断言是否登录成功并截图）
+    @allure.attach('描述', '输入正确帐号、密码，成功登录')
     def succeed_register(self):
         # 向右滑动屏幕
         for i in range(3):
